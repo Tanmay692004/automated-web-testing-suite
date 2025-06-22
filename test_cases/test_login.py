62f2fc9a636ca686a3a4d149b17ff83e610ecc42
Tanmay692004 <tanmaybinjola0@gmail.com>
2025-06-22 20:59:22 +0530

    uhhh tweaked a little

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -4,10 +4,10 @@ import pytest
 
 def test_login_success():
     driver = webdriver.Chrome()
-    driver.get("https://opensource-demo.orangehrmlive.com/")  # Testable login page
+    driver.get("https://opensource-demo.orangehrmlive.com/") 
 
     driver.find_element(By.NAME, "username").send_keys("Admin")
-    driver.find_element(By.NAME, "password").send_keys("admin123")
+    driver.find_element(By.NAME, "password").send_keys("admin123")  #acc to the website
     driver.find_element(By.TAG_NAME, "button").click()
 
     assert "dashboard" in driver.current_url.lower()
